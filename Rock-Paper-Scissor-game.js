316ba19bcd0524b17ebb1bd71f99873b5fd0d2d9
amanuelyohannes <amanuelyohannes999@gmail.com>
2024-04-15 12:45:46 +0300

    -Modify for personal reason- Last time when i make this game i copyed some code from youtube video and now i try to remake on my own.

--- Rock-Paper-Scissor-game.js
+++ Rock-Paper-Scissor-game.js
@@ -1,87 +1,115 @@
 
-let option = ["rock", "paper", "scissor"]
-let playerScore=0
-let compScore=0
-function getComputerChoice() {
-let  choice=option[Math.floor(Math.random() * option.length)];
 
-return choice
-}getComputerChoice()
+let compScore =0;
+let playerScore =0;
 
 
-// this function is where the winner is known 
-function checkWinner(playerSelection, computerSelection) {
-  
-    if(playerSelection===computerSelection) {
-      return "tie"
-   }
-   else if(playerSelection=="rock"&&computerSelection=="paper"||
-      playerSelection=="paper"&&computerSelection=="scissor"||
-      playerSelection=="scissor"&&computerSelection=="rock") {
-         return "lose"
-      }else if(playerSelection=="paper"&&computerSelection=="rock"||
-      playerSelection=="scissor"&&computerSelection=="paper"||
-      playerSelection=="rock"&&computerSelection=="scissor") { 
-        return "win" 
-        
-   }
-  
-   else{return "invalid"}
+function decideWinner (computerSelection, playerSelection) {
+
+    if(computerSelection===playerSelection){
+        return "tie";
+    }
+    else if (computerSelection==="paper" &&playerSelection==="rock"||
+              computerSelection==="rock"&& playerSelection==="scissor"||
+             computerSelection==="scissor"&& playerSelection==="paper")
+              {
+                return "lose"
+             }
+             else if (computerSelection==="rock" &&playerSelection==="paper"||
+             computerSelection==="scissor"&& playerSelection==="rock"||
+            computerSelection==="paper"&& playerSelection==="scissor"){
+                return "win";
+            }
+            else {
+               return "invalid"
+            }
+
 }
 
-// this is where each round of game is played
-function playRound (playerSelection, computerSelection) {
-   const result = checkWinner(playerSelection,computerSelection)
-      
-     if (result==="tie") {
-    return  "comp: " + computerSelection +" you: " + playerSelection + "      you tie"
-          
+  function playRound(computerSelection,playerSelection) {
 
-   } else if (result === "lose" ) {
-      return  "comp: " +computerSelection + " you: " + playerSelection+ "     you lose"
-   }
-   else if (result === "win")  { 
-      return "comp: " +computerSelection +" you: "+ playerSelection + "      you win"
+     let result =decideWinner(computerSelection,playerSelection)
 
-   }else {return "invalid input"}
-  
-} 
+     if (result==="tie"){
 
+     return `you: ${playerSelection} - comp: ${computerSelection} 
+you tied`
+  }
 
+  else if (result === "lose") {
+    return `you: ${playerSelection} - comp: ${computerSelection} 
+ you lose`
+  }
 
-// this is the whole game is played
+  else if (result === "win") {
+     return `you: ${playerSelection} - comp: ${computerSelection} 
+  you win`
+  }
+
+  else {
+     return "invalid input!"
+  }
+}
+
 function game () {
-   for (let i =0 ;i<=4 ; i++) {
-   let input =prompt("rock, paper or scissor")
-        let playerSelection = input.toLowerCase();
-     
-let computerSelection = getComputerChoice();
-   
-console.log(playRound(playerSelection,computerSelection));
 
-   
-   const score = checkWinner(playerSelection, computerSelection)
-if (score==="lose") {
-   console.log(`comp score:   ${compScore+=1}
-    your score ${playerScore} `) 
-     
-   }else if(score==="win"){
-      console.log(`your score:  ${playerScore+=1}
-       comp score: ${compScore}`)
-   }
-   else if (score==="tie") {
-      console.log(`comp score ${compScore} 
-       your score ${playerScore}`)
-   }
-  
-   }
-   if (compScore>playerScore) {
-      console.log("you lose Game over ):")
-   }else if(compScore<playerScore){
-      console.log("You win (:")
-   }else if (compScore>0&&playerScore>0&&compScore===playerScore){
-      console.log("you tie")
-   }else {console.log("BYE")}
+     for(let i = 0 ; i<=4; i++) {
+
+let getComputerChoice = function(){
+
+     let randomNumber= Math.floor(Math.random()*3+1);
+
+const number = randomNumber;
+
+if (number===1) {
+     getComputerChoice="rock"
 }
-game()
+else if (number===2) {
+     getComputerChoice="paper"
 
+}
+else{getComputerChoice="scissor"}
+
+return getComputerChoice
+
+}
+
+let input = prompt("rock, paper or scissor")
+
+let playerSelection = input.toLowerCase();
+
+const computerSelection = getComputerChoice();
+
+console.log(playRound(computerSelection,playerSelection));
+
+const score = decideWinner(computerSelection, playerSelection)
+
+  if (score==="lose") {
+     console.log(`comp score: ${compScore+=1}
+     your score: ${playerScore}`)
+  } 
+
+  else if(score==="win"){
+     console.log(`your score:  ${playerScore+=1}
+      comp score: ${compScore}`)
+  }
+
+  else if (score==="tie") {
+     console.log(`comp score: ${compScore} 
+      your score: ${playerScore}`)
+     }
+
+}
+if (compScore>playerScore){
+     console.log("you lose the game ):")
+} 
+else if (compScore<playerScore) {
+     console.log("you win the game (:")
+}
+
+else if (compScore===playerScore) {
+     console.log("you tied! play again?")
+}
+
+ }
+  game()
